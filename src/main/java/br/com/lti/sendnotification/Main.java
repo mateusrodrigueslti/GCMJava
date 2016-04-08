120de09c244d06a09462cb138bca4462e3b4448b
Mateus Rodrigues <omateus2004@gmail.com>
2016-04-08 19:17:55 -0300

    Update Main.java

--- src/main/java/br/com/lti/sendnotification/Main.java
+++ src/main/java/br/com/lti/sendnotification/Main.java
@@ -8,9 +8,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String deviceToken = "APA91bE6xQZh7dV9S5kg2TLsykVA7WhCjNREuQAJbQytXyNXCarypxLp-xrOR_ELnUjys796mwHdTwp5teFQSJGTJReNohORy9JD1S_pWegdE0Vy0xKFttO3oni4omj3EYeMUXFzox4y";
+        String deviceToken = "Your Token here";
         
-        String apiKey = "AIzaSyDmV3M7wjVe4I2RwohOk3G2i6GduIaLNSI";
+        String apiKey = "API KEY";
         String registrationId = deviceToken; 
         Sender sender = new Sender(apiKey);
         Message message = new Message.Builder()
